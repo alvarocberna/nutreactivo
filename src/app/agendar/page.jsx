5d4370b2694129f2defb4c29cbcb472789eb1939
Alvaro <cberna.alvaro@gmail.com>
2026-06-10 20:26:50 -0400

    refactor: seccion agendar refactorizada

--- src/app/agendar/page.jsx
+++ src/app/agendar/page.jsx
@@ -1,21 +1,48 @@
 
 import './style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+
 
 export default function Agendar() {
     return (
-        <div className="d-flex flex-column" style={{ marginTop: '150px', marginBottom: '200px' }}>
-            <div className='m-auto d-flex flex-column col-10 col-sm-8 col-md-10 col-lg-11 col-xl-9'>
-                <h2 className="m-auto mb-5 h1">
-                    Agenda tu hora
-                </h2>
-                <p className='m-auto mb-5'>
-                    Haz click en el botón <b>agendar</b> para ser re dirigido a whatsapp.
-                </p>
-                <a className="m-auto col-12 col-md-8 h4 btnAgeSty" target='_blank' rel='noreferrer'
-                    href="https://api.whatsapp.com/send?phone=56987307068&text=¡Hola! Quiero agendar una hora para consulta nutricional">
-                    Agendar
-                </a>
+        <section className="agendarSection">
+
+            <div className="agendarHeader">
+                <span className="agendarBadge">
+                    <FontAwesomeIcon icon={faWhatsapp} />
+                    Agenda por WhatsApp
+                </span>
+                <h1 className="agendarTitle">Agenda tu consulta</h1>
             </div>
-        </div>
-    );
+
+
+            <div className="agendarCard">
+                <div className="agendarCardAccent" />
+                <div className="agendarCardBody">
+                    <div className="agendarWaIcon">
+                        <FontAwesomeIcon icon={faWhatsapp} />
+                    </div>
+                    <h2 className="agendarCardTitle">¿Listo para comenzar?</h2>
+                    <p className="agendarCardDesc">
+                        Haz click en el botón para abrir WhatsApp y agendar tu consulta nutricional personalizada.
+                    </p>
+                    <a
+                        className="agendarBtn"
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://api.whatsapp.com/send?phone=56987307068&text=¡Hola! Quiero agendar una hora para consulta nutricional"
+                    >
+                        <FontAwesomeIcon icon={faWhatsapp} />
+                        Agendar por WhatsApp
+                    </a>
+                    {/* <p className="agendarCardNote">
+                        <FontAwesomeIcon icon={faClock} />
+                        Tiempo de respuesta: menos de 24 horas hábiles
+                    </p> */}
+                </div>
+            </div>
+
+        </section>
+    )
 }
