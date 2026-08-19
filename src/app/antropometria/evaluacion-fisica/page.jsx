bf5692627eb94801839468dd814fcc9c542ae884
Alvaro <cberna.alvaro@gmail.com>
2026-08-19 17:16:49 -0400

    refactor: rutas 'consulta-nutricional' y 'evaluacion-fisica' refactorizadas

--- src/app/antropometria/evaluacion-fisica/page.jsx
+++ src/app/antropometria/evaluacion-fisica/page.jsx
@@ -1,88 +1,114 @@
+'use client'
 
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { InfoIndicadores } from './datos';
 import './style.css';
 
-function Indicador(props) {
-    let titulo = props.titulo;
-    let img = props.img;
-    let cuerpo = props.cuerpo;
-    let idInd = props.idInd;
-    let targetInd = props.targetInd;
-    let idBox = props.idBox;
-    let targetBox = props.targetBox;
+function Indicador({ titulo, img, cuerpo }) {
+    const [open, setOpen] = useState(false)
     return (
-        <div className="accordion px-0 col-12 col-md-5 col-xl-3 mx-1 mb-3 mb-sm-5 rounded" style={{ position: 'relative' }} id={idBox}>
-            <div className="accordion-item py-3 d-flex flex-column">
-                <h2 className="accordion-header h4 text-tertiary text-center" style={{ height: '60px' }}>
-                    {titulo}
-                </h2>
-                <Image src={img} alt='Indicador deportivo evaluación nutricional' height={250} width={250} className='m-auto' />
-                <button className="col-10 px-3 py-1 bg-secondary text-white m-auto rounded" style={{ border: 'none' }} type="button" data-bs-toggle="collapse" data-bs-target={targetInd} aria-expanded="false" aria-controls={idInd}>
-                    Ver detalles
-                </button>
-                <div id={idInd} className="accordion-collapse collapse" data-bs-parent={targetBox}>
-                    <p className='mx-3 my-3' style={{ textAlign: 'justify' }}>{cuerpo}</p>
+        <div className={`evalIndicadorCardSty ${open ? 'is-open' : ''}`}>
+            <div className='evalFlipSceneSty'>
+                <div className='evalFlipCardSty'>
+                    <div className='evalFlipFaceSty evalFlipFrontSty'>
+                        <Image src={img} alt={titulo} width={220} height={220} style={{ objectFit: 'contain' }} />
+                    </div>
+                    <div className='evalFlipFaceSty evalFlipBackSty'>
+                        <p className='evalIndicadorDescSty'>{cuerpo}</p>
+                    </div>
                 </div>
             </div>
+            <h3 className='evalIndicadorTitleSty'>{titulo}</h3>
+            <button type='button' className='evalIndicadorToggleBtn' onClick={() => setOpen((o) => !o)} aria-expanded={open}>
+                {open ? 'Ver imagen' : 'Ver detalles'}
+                <FontAwesomeIcon icon={faChevronDown} className='evalChevronSty' />
+            </button>
         </div>
     )
 }
 
 export default function EvaluacionFisica() {
     return (
-        <div className='d-flex flex-column mb-5'>
-            <h2 className='h1 m-auto col-10 col-sm-8 col-md-10 col-lg-11 col-xl-9 mb-5'>Evaluación física</h2>
+        <div className='w-100 d-flex flex-column py-5'>
+            <div className='m-auto d-flex flex-column col-10 col-sm-8 col-md-10 col-lg-11 col-xl-9' style={{ gap: '48px' }}>
 
-            <div className=' col-10 col-lg-9 d-flex flex-column-reverse flex-md-row-reverse justify-content-between m-auto mb-5'>
-                <div className='col-12 col-md-5 col-lg-4 mb-3 mb-md-0 d-flex justify-content-center'>
-                    <iframe height="250px" className='col-12' style={{ borderRadius: '20px' }}
-                        src="https://www.youtube.com/embed/w5tOpQVNeOQ">
-                    </iframe>
-                </div>
-                <div className='col-12 col-md-6 col-lg-7 d-flex flex-column justify-content-center '>
-                    <h3 className=''>¿En qué consiste la evaluación?</h3>
-                    <p className='' style={{ textAlign: 'justify' }}>
-                        Durante la evaluación física el nutricionista realizará la toma de mediciones
-                        corporales como lo son los pliegues, perímetros, diámetros óseos, peso y talla,
-                        todo según el estándar internacional ISAK I. Esta evaluación tiene una duración de
-                        10 minutos.
+                <div className='evalHeaderSty'>
+                    <h1 className='h1 mb-2 text-tertiary fw-bold'>Evaluación física</h1>
+                    <p className='text-muted mb-0'>
+                        Conoce en qué consiste nuestra evaluación antropométrica y todos los indicadores
+                        deportivos que obtendrás en tu informe de resultados.
                     </p>
+                </div>
+
+                <div className='evalIntroRow'>
+                    <div className='evalIntroVideo'>
+                        <div className='evalVideoCardSty'>
+                            <div className='evalVideoRatioSty'>
+                                <iframe
+                                    src='https://www.youtube.com/embed/w5tOpQVNeOQ'
+                                    title='En qué consiste la evaluación física'
+                                    allowFullScreen
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='evalIntroText'>
+                        <h2 className='h3'>¿En qué consiste la evaluación?</h2>
+                        <p>
+                            Durante la evaluación física el nutricionista realizará la toma de mediciones
+                            corporales como lo son los pliegues, perímetros, diámetros óseos, peso y talla,
+                            todo según el estándar internacional ISAK I. Esta evaluación tiene una duración de
+                            10 minutos.
+                        </p>
+                        <p>
+                            Al finalizar la evaluación se le entrega al usuario un informe con los resultados
+                            de sus mediciones, los cuales incluyen:
+                        </p>
+                        <ul className='evalChecklistSty'>
+                            <li>
+                                <span className='evalChecklistIconSty'><FontAwesomeIcon icon={faCheck} /></span>
+                                <span>Composición corporal (masa muscular y grasa corporal)</span>
+                            </li>
+                            <li>
+                                <span className='evalChecklistIconSty'><FontAwesomeIcon icon={faCheck} /></span>
+                                <span>Indicadores de uso deportivo como somatotipo y modelo phantom</span>
+                            </li>
+                            <li>
+                                <span className='evalChecklistIconSty'><FontAwesomeIcon icon={faCheck} /></span>
+                                <span>Indicadores de proporcionalidad, entre otros</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className='evalResultadosIntroSty'>
+                    <h2 className='h3 mb-3'>Resultados mediciones</h2>
                     <p>
-                        Al finalizar la evaluación se le entrega al usuario un informe con los resultados
-                        de sus mediciones, los cuales incluyen composición corporal (masa muscular y grasa corporal),
-                        indicadores de uso deportivo como somatotipo, modelo phantom, indicadores de proporcionalidad,
-                        entre otros.
+                        La evaluación física o antropométrica nos entrega como resultado una gran variedad de
+                        indicadores de interés deportivo y de salud, como lo son los componentes corporales,
+                        somatotipo, índice de masa corporal, entre otros indicadores físicos.
                     </p>
                 </div>
-            </div>
 
-            <div className='row m-auto col-10 col-sm-8 col-md-10 col-lg-11 col-xl-9 mb-3 d-flex flex-column mb-5'>
-                <h4 className='h3 mb-4'>Resultados mediciones</h4>
-                <p className=''>
-                    La evaluación física o antropométrica nos entrega como resultado una gran variedad de
-                    indicadores de interés deportivo y de salud, como lo son los componentes corporales,
-                    somatotipo, índice de masa corporal, entre otros
-                    indicadores físicos.
-                </p>
-            </div>
+                <div className='evalIndicadorGridSty'>
+                    <Indicador {...InfoIndicadores.componentesPorc} />
+                    <Indicador {...InfoIndicadores.componentesKg} />
+                    <Indicador {...InfoIndicadores.somatocarta} />
+                    <Indicador {...InfoIndicadores.phantom} />
+                    <Indicador {...InfoIndicadores.imc} />
+                    <Indicador {...InfoIndicadores.ffmi} />
+                    <Indicador {...InfoIndicadores.pliegues} />
+                    <Indicador {...InfoIndicadores.perimetros} />
+                    <Indicador {...InfoIndicadores.sumatorias} />
+                    <Indicador {...InfoIndicadores.indiceCormico} />
+                    <Indicador {...InfoIndicadores.indiceEsquelico} />
+                    <Indicador {...InfoIndicadores.envergadura} />
+                </div>
 
-            <div className='row m-auto col-10 col-sm-8 col-md-10 col-lg-11 col-xl-10 justify-content-around'>
-                <Indicador {...InfoIndicadores.componentesPorc} />
-                <Indicador {...InfoIndicadores.componentesKg} />
-                <Indicador {...InfoIndicadores.somatocarta} />
-                <Indicador {...InfoIndicadores.phantom} />
-                <Indicador {...InfoIndicadores.imc} />
-                <Indicador {...InfoIndicadores.ffmi} />
-                <Indicador {...InfoIndicadores.pliegues} />
-                <Indicador {...InfoIndicadores.perimetros} />
-                <Indicador {...InfoIndicadores.sumatorias} />
-                <Indicador {...InfoIndicadores.indiceCormico} />
-                <Indicador {...InfoIndicadores.indiceEsquelico} />
-                <Indicador {...InfoIndicadores.envergadura} />
             </div>
-
         </div>
     )
 }
